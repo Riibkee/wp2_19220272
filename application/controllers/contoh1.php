18e2605ff5d68c930b78079ea50385c7a4801fab
Riibkee <19220272.bsi.ac.id>
2023-10-13 15:34:00 +0700

    penambahan file baru

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php 
+class Contoh1 extends CI_Controller 
+{ 
+    public function index() 
+    { 
+        echo "<h1>Perkenalkan</h1>"; 
+        echo"Nama saya Ribka Dolok Saribu<br>
+            Saya tingga di daerah Bekasi Utara<br> 
+            olah raga yang saya sukai adalah <br>
+            Bulutangkis";
+    }
+}
